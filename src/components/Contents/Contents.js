5881c2f0f56f3c7406f570058c6e1e9e1007df5a
Dilip <dilippoudel578@gmail.com>
2022-11-26 22:43:21 +0200

    Calculate Toal number of exercises

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,11 +1,17 @@
 import Part from '../Part/Part'
 
 const Contents = ({ contents }) => {
+  const totalExe = contents.map((c) => c.exercises)
+  const sum = totalExe.reduce((a, b) => a + b)
+  console.log(sum)
   return (
     <div>
       {contents.map((content) => (
         <Part items={content} key={content.id} />
       ))}
+      <p>
+        <b>Total of {sum} exercises</b>
+      </p>
     </div>
   )
 }
