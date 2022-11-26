31b902780140dc761cd8a1a63af15cde3cfd2aa4
Dilip <dilippoudel578@gmail.com>
2022-11-27 01:19:07 +0200

    Add contact number of person

--- src/App.js
+++ src/App.js
@@ -1,36 +1,70 @@
 import { useState } from 'react'
 const Header = ({ text }) => <h2>{text}</h2>
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '123' },
+  ])
+  const [newContact, setNewContact] = useState({ name: '', number: '' })
 
   const addPerson = (e) => {
     e.preventDefault()
-    const isAlreadyExist = persons.find((person) => person.name === newName)
+    const isAlreadyExist = persons.find(
+      (person) => person.name === newContact.name,
+    )
     if (isAlreadyExist) {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newContact.name} is already added to phonebook`)
       return
     }
-    setPersons((prevState) => [...prevState, { name: newName }])
-    setNewName('')
+    setPersons((prevState) => [
+      ...prevState,
+      { name: newContact.name, number: newContact.number },
+    ])
+    setNewContact({ name: '', number: '' })
   }
-  const changeNameHandler = (e) => setNewName(e.target.value)
-
+  const changeNameHandler = (e) => {
+    let createNew = {
+      ...newContact,
+      name: e.target.value,
+    }
+    setNewContact(createNew)
+  }
+  const changeNumberHandler = (e) => {
+    let createNew = {
+      ...newContact,
+      number: e.target.value,
+    }
+    setNewContact(createNew)
+  }
   return (
     <div>
       <Header text="PhoneBook" />
       <form onSubmit={addPerson}>
         <div>
-          <input type="text" value={newName} onChange={changeNameHandler} />
-          <div>
-            <button type="submit">add</button>
-          </div>
+          Name:{' '}
+          <input
+            type="text"
+            value={newContact.name}
+            onChange={changeNameHandler}
+          />
+        </div>
+        <div>
+          Number:{' '}
+          <input
+            type="text"
+            value={newContact.number}
+            onChange={changeNumberHandler}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
         </div>
       </form>
       <Header text="Number" />
       <div>
         {persons.map((person) => (
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
         ))}
       </div>
     </div>
