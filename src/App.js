9fce481b8e692e16db26b2be5c40926d552d40d4
Dilip <dilippoudel578@gmail.com>
2022-11-27 00:46:55 +0200

    Add namelist to phone book

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
+import { useState } from 'react'
+const Header = ({ text }) => <h2>{text}</h2>
 const App = () => {
-  return <div>Phone book App</div>
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    setPersons((prevState) => [...prevState, { name: newName }])
+    setNewName('')
+  }
+  const changeNameHandler = (e) => setNewName(e.target.value)
+
+  return (
+    <div>
+      <Header text="PhoneBook" />
+      <form onSubmit={addPerson}>
+        <div>
+          <input type="text" value={newName} onChange={changeNameHandler} />
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </div>
+      </form>
+      <Header text="Number" />
+      <div>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 export default App
