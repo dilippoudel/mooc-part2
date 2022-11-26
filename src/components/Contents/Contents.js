3ba33e84beb9940f8be52227e3dee6d208cfb66c
Dilip <dilippoudel578@gmail.com>
2022-11-26 22:49:26 +0200

    Modify sum of total exercise function

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,16 +1,17 @@
 import Part from '../Part/Part'
 
 const Contents = ({ contents }) => {
-  const totalExe = contents.map((c) => c.exercises)
-  const sum = totalExe.reduce((a, b) => a + b)
-  console.log(sum)
+  const totalExercises = contents
+    .map((c) => c.exercises)
+    .reduce((a, b) => a + b)
+
   return (
     <div>
       {contents.map((content) => (
         <Part items={content} key={content.id} />
       ))}
       <p>
-        <b>Total of {sum} exercises</b>
+        <b>Total of {totalExercises} exercises</b>
       </p>
     </div>
   )
