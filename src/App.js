9cf1136d0ea491847b18db1e452ed0a3d5debd09
Dilip <dilippoudel578@gmail.com>
2022-12-03 10:59:39 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -20,16 +20,14 @@ const App = () => {
     setFilteredCountries(data)
   }
 
-  useEffect(filtering, [searchText])
-
   const renderingCountries = () => {
-    // if (searchText.length === 0) {
-    //   return
-    // }
+    if (searchText.length === 0) {
+      return
+    }
     if (filteredCountries.length > 10) {
       return <p>too many render</p>
     }
-    if (filteredCountries.length < 10) {
+    if (filteredCountries.length < 10 && filteredCountries.length > 1) {
       return (
         <div>
           {filteredCountries.map((c, i) => (
@@ -39,26 +37,34 @@ const App = () => {
       )
     }
     if (filteredCountries.length === 1) {
-      return (
-        <div>
-          {filteredCountries.map((country, i) => {
-            console.log(country)
-            return (
-              <div key={i}>
-                <h4>{country.name.common}</h4>
-                <h5>Languages</h5>
-                {/* I want render the list of all languages of a specific county:
-                For eg, When I enter swi in search text box, it should render 4 languages:   */}
-                {/* {country.languages.map((l) => console.log(l))} */}
-              </div>
-            )
-          })}
-        </div>
-      )
+      console.log('l are ', filteredCountries[0].languages)
+
+      // return (
+      // <div>
+      //   {filteredCountries.map((country, i) => {
+      //     console.log('counties in filter', country)
+
+      //     return (
+      //       <div key={i}>
+      //         <h4>{country.name.common}</h4>
+      //         <h5>Languages</h5>
+      //         {/* I want render the list of all languages of a specific county:
+      //         For eg, When I enter swi in search text box, it should render 4 languages:   */}
+      //         {/* {country.languages.map((l) => console.log(l))} */}
+      //         {console.log('languages, ', country.languages)}
+      //         <h2>hello</h2>
+      //       </div>
+      //     )
+      //   })}
+      // </div>
+
+      // )
     }
     return
   }
   useEffect(hooks, [])
+  useEffect(filtering, [searchText, countries])
+
   return (
     <div>
       <div>
